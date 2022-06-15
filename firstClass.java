7e0897ee09bce344658723457efb511b02f1fca1
Tanya Raj <73035345+TANYARAJ11@users.noreply.github.com>
2022-06-15 20:58:44 +0530

    Add files via upload
    Area of the circle with radius given by the user

---
+++ firstClass.java
@@ -0,0 +1,14 @@
+
+import java.net.SocketTimeoutException;
+import java .util.*;
+ public class firstClass {
+    public static void main(String args[]) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the radius of circle6");
+        float Radius = sc.nextFloat();
+        float Area = Radius*Radius;
+        System.out.println(Area);
+        
+      
+}
+}
